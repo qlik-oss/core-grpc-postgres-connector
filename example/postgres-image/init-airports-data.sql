8ed4fa5e39ba8192839bc7d0d12d9a66173c8619
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-10-24 13:57:00 +0200

    cleaned up the postgres parts

---
+++ example/postgres-image/init-airports-data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.airports
+(
+  rowid integer,
+  airport character varying(250),
+  city character varying(250),
+  country character varying(250),
+  iatacode character varying(50),
+  icaocode character varying(50),
+  latitude character varying(50),
+  longitude character varying(50),
+  altitude character varying(50),
+  timezone character varying(50),
+  dst character varying(50),
+  tz character varying(50)
+)
+COPY airports(rowID,Airport,City,Country,IATACode,ICAOCode,Latitude,Longitude,Altitude,TimeZone,DST,TZ) FROM '/airports.csv' DELIMITER ',' CSV HEADER;
