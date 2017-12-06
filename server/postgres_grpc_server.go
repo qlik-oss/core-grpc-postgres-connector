d931757f97a71a53d70a6cb83be799fbceab9a96
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-12-06 11:06:38 +0100

    Now sending a correct gRPC error if reading from postgres fails (#25)
    * sending correct grpc errors with error code internal and the message from postgres

--- server/postgres_grpc_server.go
+++ server/postgres_grpc_server.go
@@ -4,21 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"runtime/pprof"
 	"time"
 
+	"github.com/qlik-ea/postgres-grpc-connector/postgres"
 	"github.com/qlik-ea/postgres-grpc-connector/qlik"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	"github.com/qlik-ea/postgres-grpc-connector/postgres"
-)
-
-const (
-	port = ":50051"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
@@ -38,6 +32,7 @@ func (s *server) ExecuteGenericCommand(context context.Context, genericCommand *
 func (s *server) GetData(dataOptions *qlik.GetDataOptions, stream qlik.Connector_GetDataServer) error {
 
 	flag.Parse()
+
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
@@ -51,11 +46,12 @@ func (s *server) GetData(dataOptions *qlik.GetDataOptions, stream qlik.Connector
 	}
 
 	var t0 = makeTimestamp()
-
 	var connectionString = dataOptions.Connection.ConnectionString
+
 	if dataOptions.Connection.User != "" {
 		connectionString = connectionString + ";user=" + dataOptions.Connection.User
 	}
+
 	if dataOptions.Connection.Password != "" {
 		connectionString = connectionString + ";password=" + dataOptions.Connection.Password
 	}
@@ -63,36 +59,21 @@ func (s *server) GetData(dataOptions *qlik.GetDataOptions, stream qlik.Connector
 	if s.postgresReaders[connectionString] == nil {
 		fmt.Println("Starting connection pool")
 		fmt.Println(connectionString)
-		var err2 error
-		s.postgresReaders[connectionString], err2 = postgres.NewPostgresReader(connectionString)
-		if err2 != nil {
-			return err2
+		reader, err := postgres.NewPostgresReader(connectionString)
+		s.postgresReaders[connectionString] = reader
+		if err != nil {
+			return status.Error(codes.Internal, err.Error())
 		}
 	} else {
 		fmt.Println("Reusing connection pool")
 	}
-	var getDataErr = s.postgresReaders[connectionString].GetData(dataOptions, stream)
 
-	var t1 = makeTimestamp()
-	fmt.Println("Time", t1-t0, "ms")
-	return getDataErr
-}
-
-func main() {
-	lis, err := net.Listen("tcp", port)
+	err := s.postgresReaders[connectionString].GetData(dataOptions, stream)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		err = status.Error(codes.Internal, err.Error())
 	}
+	t1 := makeTimestamp()
+	fmt.Println("Time", t1-t0, "ms")
 
-	s := grpc.NewServer()
-	var srv = &server{make(map[string]*postgres.Reader)}
-	qlik.RegisterConnectorServer(s, srv)
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	fmt.Println("Server started", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-		return
-	}
-
+	return err
 }
